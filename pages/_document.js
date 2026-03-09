6066a052c60d148bf710615974c4c2d933672d8d
rintuchowdory <chowdoryrintu60@gmail.com>
2026-03-09 19:06:27 +0100

    Fix: Use Tailwind CDN instead of PostCSS

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <script src="https://cdn.tailwindcss.com"></script>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
